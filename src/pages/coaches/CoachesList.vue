a64f2ee44c01c0d9c10effff55eeb25a30246241
Dominik Wojtowicz <dwojtowicz@mbtmedia.pl>
2022-08-15 20:52:58 +0200

    basic templates added

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul></ul>
+    LIST OF COACHES
+  </section>
+</template>
